fbee47136293341459d8d0f64d23f3646dd2d7dc
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-09 19:32:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,6 +21,15 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
+	monopoly = {:railroads => {
+    :pieces => 4, 
+    :names => {
+      :reading_railroad => {}
+    }, 
+    :rent_in_dollars => {}
+    }
+  }
+	
 	monopoly = {}
   monopoly[:railroads] = {}
   monopoly[:railroads][:pieces] = 4
